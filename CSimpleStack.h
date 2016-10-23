1d64a4813d61b578e18e28b93c3012e585ed4282
Alexey Turchaninov <AXLiberty@users.noreply.github.com>
2016-10-23 11:35:24 +0300

    Update CSimpleStack.h

--- CSimpleStack.h
+++ CSimpleStack.h
@@ -7,19 +7,19 @@ template <typename T>
 class CSimpleStack : public std::deque<T>
 {
 private:
-	uint64_t m_u64Max;
+	uint64_t m_u64MaxCount;
 
 public:
-	CSimpleStack(uint64_t maxCount = 1) { m_u64Max = maxCount; }
-	void setMaxCount(uint64_t maxCount) { m_u64Max = maxCount; }
-	auto getMaxCount() const { return m_u64Max; }
+	CSimpleStack(uint64_t maxCount = 1) { m_u64MaxCount = maxCount; }
+	void setMaxCount(uint64_t maxCount) { m_u64MaxCount = maxCount; }
+	auto getMaxCount() const { return m_u64MaxCount; }
 
 	auto count() const { return __super::size(); }
 	auto size() const { return count() * sizeof(T); }
 
 	void push(T value)
 	{
-		if (count() >= m_u64Max)
+		if (count() >= m_u64MaxCount)
 			pop_front();
 		push_back(value);
 	}
